29c1fed73a4262756020ace5a2f6acfd6fe0ffaf
Catherine Smith <catsmith24601@gmail.com>
2021-07-10 13:43:29 -0400

    played with adding components to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-  
+      <NavBar />
+      <Header />
+      <Home />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
 }
