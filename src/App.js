9151d3ff81f995ed7a8e2696a7c470382e044fb1
Catherine Smith <catsmith24601@gmail.com>
2021-07-13 09:46:18 -0400

    added jumbotron

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 import NavBar from './components/NavBar';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Jumbotron>
+        <h1>Hello, I'm Catherine!</h1>
+        <p>
+          Full Stack Web Developer
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>      
       <Header />
       <Home />
       <Portfolio />
