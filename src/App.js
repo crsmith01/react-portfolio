11a22002f38ffc927f9aec1d2b976ef3f2545811
Catherine Smith <catsmith24601@gmail.com>
2021-07-13 10:05:56 -0400

    added <Switch> and <Router> to App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import Home from './components/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Contact from './components/Contact';
-import NavBar from './components/NavBar';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Jumbotron>
-        <h1>Hello, I'm Catherine!</h1>
-        <p>
-          Full Stack Web Developer
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </Jumbotron>      
-      <Header />
-      <Home />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header /> 
+        <Jumbotron>
+          <h1>Hello, I'm Catherine!</h1>
+          <p>
+            Full Stack Web Developer. Librarian. Soprano. Shakespeare Nerd.
+          </p>
+          <Switch>
+            {/* <Switch> Renders the first child <Route> or <Redirect> that matches the location. <Switch> is unique in that it renders a route exclusively. In contrast, every <Route> that matches the location renders inclusively. */}
+            <Route exact path='/' component={Home} />
+            <Route exact path='/portfolio' component={Portfolio} />
+            {/* All other paths will lead home */}
+            <Route path='/*' component={Home} />
+          </Switch>
+        </Jumbotron>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
