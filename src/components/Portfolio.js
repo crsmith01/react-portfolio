8ad47aec182b3fd02bd500baca7d75351713a226
Catherine Smith <catsmith24601@gmail.com>
2021-07-13 10:23:56 -0400

    set up useState and array of projects in Portfolio.js

---
+++ src/components/Portfolio.js
@@ -0,0 +1,68 @@
+// import `useState` with React so that we can utilize the hook
+import React, { useState } from 'react';
+
+const Portfolio = () => {
+    const [projects, setProjects] = useState([
+        {
+            id: 1, 
+            name: ,
+            description: ,
+            img: ,
+            alt: ,
+            repo: ,
+            deployedApp: ,
+            techUsed: []
+        },
+        {
+            id: 2, 
+            name: ,
+            description: ,
+            img: ,
+            alt: ,
+            repo: ,
+            deployedApp: ,
+            techUsed: []
+        },
+        {
+            id: 3, 
+            name: ,
+            description: ,
+            img: ,
+            alt: ,
+            repo: ,
+            deployedApp: ,
+            techUsed: []
+        },
+        {
+            id: 4, 
+            name: ,
+            description: ,
+            img: ,
+            alt: ,
+            repo: ,
+            deployedApp: ,
+            techUsed: []
+        },
+        {
+            id: 5, 
+            name: ,
+            description: ,
+            img: ,
+            alt: ,
+            repo: ,
+            deployedApp: ,
+            techUsed: []
+        },
+        {
+            id: 6, 
+            name: ,
+            description: ,
+            img: ,
+            alt: ,
+            repo: ,
+            deployedApp: ,
+            techUsed: []
+        },
+
+    ]);
+};
