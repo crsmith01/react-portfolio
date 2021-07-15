91313b2de2b7c63594728d875013f2c7a76ea117
Catherine Smith <catsmith24601@gmail.com>
2021-07-15 13:38:59 -0400

    updated navbar links, started playing with react tooltips

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,30 +9,70 @@ import Contact from './Contact';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { LinkContainer } from 'react-router-bootstrap';
+import ReactTooltip from 'react-tooltip';
+// need to adjust tooltip to reacttooltip
+https://www.npmjs.com/package/react-tooltip
+
 
 
 export const NavBar = () => {
 
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
+            {/* either eliminate .Brand or add CRS logo here instead */}
             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                <Nav>
-                    <Nav.Link href="#deets">More deets</Nav.Link>
-                    <Nav.Link eventKey={2} href="#memes">
-                        Dank memes
+                    {/* Links for Home, Portfolio, Contact, Resume, and Relevant Socials */}
+                    <Nav.Link href="#home">
+                        Home
+                    </Nav.Link>
+
+                    <Nav.Link href="#portfolio">
+                        Portfolio
+                    </Nav.Link>
+
+                    <Contact />
+
+                    <Nav.Link
+                        className='link'
+                        href="https://docs.google.com/document/d/e/2PACX-1vTOmmrXw5f6mpnJ4ntzogEC_U2VBiDCNWEIMASmboPkLAYC9ApUQoJoF1oIFBqTWVA4OQbMzuODDxsM/pub"
+                        // opens link in new tab or window
+                        target='_blank'
+                        // noreferrer - to prevent knowing where user came from  - Google Analytics Direct traffic instaed of Referral traffic
+                        // noopener - disables the JS window.opener property for increased securtity - best practice?
+                        rel='noreferrer noopener'
+
+                    >
+
+                        Resume
+                    </Nav.Link>
+
+                    <Nav.Link
+                        href='https://github.com/crsmith01'
+                        target='_blank'
+                        className='links'
+                        data-toggle="tooltip" 
+                        title="GitHub Profile"
+                        data-placement="bottom"
+                        rel='noreferrer noopener'>
+                        <span className='icon'>
+                            <i className='fab fa-github'></i>
+                        </span>
+                    </Nav.Link>
+
+                    <Nav.Link
+                        href='https://www.linkedin.com/in/catherine-smith24601'
+                        target='_blank'
+                        className='links'
+                        data-toggle="tooltip" 
+                        title="LinkedIn Profile"
+                        data-placement="bottom"
+                        rel='noreferrer noopener'>
+                        <span className='icon'>
+                            <i className='fa fa-3x fa-linkedin-square'></i>
+                        </span>
                     </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
