6612f91c1f76289a20daa87c43ab75dc0c4f037e
Catherine Smith <catsmith24601@gmail.com>
2021-07-20 20:03:27 -0400

    added export to portfolio js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,14 @@
 import React from 'react';
 
 const Footer = () => {
-   return ( 
-   <footer className='acknowledgements' fixed='bottom'>
-       <p>
-           Designed by Catherine Smith | Made with React <i className='fab fa-react'></i> | Copyright © 2021 |     1. Photo by <a href="https://freeimages.com/photographer/amandafoe-48011">Amanda Dafoe</a> from <a href="https://freeimages.com">FreeImages</a>
-           {/* update with whichever background photo I choose */}
-       </p>
-    </footer>
-   );
+    return (
+        <footer className='acknowledgements' fixed='bottom'>
+            <p>
+                Designed by Catherine Smith | Made with React <i className='fab fa-react'></i> | Copyright © 2021 |     1. Photo by <a href="https://freeimages.com/photographer/amandafoe-48011">Amanda Dafoe</a> from <a href="https://freeimages.com">FreeImages</a>
+                {/* update with whichever background photo I choose */}
+            </p>
+        </footer>
+    );
 };
 
 export default Footer;
