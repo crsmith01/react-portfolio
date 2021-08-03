0f3c27515ac3bc7a0c54c312f7cb05ef1f84aaa8
Catherine Smith <catsmith24601@gmail.com>
2021-08-03 10:29:48 -0400

    added technologies lists

---
+++ src/components/Technologies.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export const Technologies = () => {
+    return (
+        <Container className='techTable'>
+            <Row className='techCategories'>
+                <Col>
+                    <h2>Programming Languages</h2>
+                </Col>
+                <Col>
+                    <h2>Programming Tools</h2>
+                </Col>
+                <Col>
+                    <h2>Library Science Technology</h2>
+                </Col>
+                <Col>
+                    <h2>Other Technology</h2>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <ul className='techList'>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>MySQL</li>
+                        <li>NoSQL</li>
+                    </ul>
+                </Col>
+                <Col>
+                    <ul className='techList'>
+                        <li>GitHub & Git Pages * Git</li>
+                        <li>Heroku & Digital Oceans</li>
+                        <li>Bootstrap & Bulma & Semantic UI</li>
+                        <li>Insomnia & Postman</li>
+                        <li>MySQL & Sequelize</li>
+                        <li>MongoDB & Mongoose & Robo 3T</li>
+                        <li>jQuery</li>
+                        <li>RESTful APIs</li>
+                        <li>MERN</li>
+                        <li>MVC file structure</li>
+                        <li>Node.js</li>
+                        <li>Express.js</li>
+                        <li>Handlebars</li>
+                        <li>Moment</li>
+                        <li>Font Awesome & Google Fonts</li>
+                        <li>Sweetalerts</li>
+                        <li>Progressive Web Apps (PWAs)</li>
+                        <li>TypeDefs and Resolvers</li>
+                        <li>Queries and Mutations</li>
+                        <li>Manifest.json & Service workers</li>
+                        <li>GraphQL & Apollo Server</li>
+                        <li>Authentication (JWT)</li>
+                    </ul>
+                </Col>
+                <Col>
+                    <ul className='techList'>
+                        <li>Library of Congress Classification</li>
+                        <li>Dewey Decimal Classification</li>
+                        <li>Superindendent of Docs</li>
+                        <li>Biblioteca</li>
+                        <li>Infolinx</li>
+                        <li>Alma, SUPrimo, Primo, Leganto from Ex Libris</li>
+                        <li>Metadata Schemas: MARC, ISBD, RDA, AACR2</li>
+                        <li>Koha</li>
+                        <li>WorldCat</li>
+                        <li>OCLC</li>
+
+                    </ul>
+                </Col>
+                <Col>
+                    <ul className='techList'>
+                        <li>Google Suite</li>
+                        <li>Microsoft Office Suite</li>
+                        <li>Noteflight</li>
+                        <li>Wordpress</li>
+                        <li>QuickBooks</li>
+                        <li>Typing Speed: 81 wpm with 98% accuracy</li>
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+
+export default Technologies;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,45 +3,51 @@ import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Technologies from '../Technologies';
 
 
 const Home = () => {
     return (
-        <Container>
-            <Row>
-                <div id='aboutMe'>
-                    <section id='aboutMeContainer'>
-                        <Image
-                            src='../public/images/images-tinified/catherine-headshot-sunken-garden.png'
-                            alt='Catherine Smith headshot'
-                            className='headshot'
-                            roundedCircle
-                        />
-                        <div id='aboutMeText'>
-                            <h1><u> About Me </u></h1>
-                            <br></br>
-                            <p>
-                                My name is Catherine, and I am an emerging full-stack Javascript web developer currently completing the
-                                University of Richmond's Coding Bootcamp. After earning my BA in Music and Shakespeare Studies
-                                at the College of William and Mary, I ventured to Glasgow to acquire my MSc in Information and Library Studies at the
-                                University of Strathclyde. I decided to pursue web development because it's a fast-paced,
-                                challenging, creative career path with constant opportunities to grow as a person and professional.
-                            </p>
-                            <br></br>
-                            <p>
-                                While volunteering time as a Library Ambassador at the Earl Gregg Swem Library, I worked with
-                                the media team to create virtual tours and how-to videos for utilizing library services, and I
-                                contacted professors and students to act as a reference point if they had any questions about how to
-                                utilize library resources at William and Mary. Ensuring that people know how to best access and utilize the resources at
-                                their disposal is a passion of mine. I enjoy studying new subjects, learning German, drinking tea, singing, watching LOTR, and
-                                listening to audiobooks (particularly if they are narrated by Richard Armitage, Elizabeth Wiley, Jim Dale, or
-                                Stephen Fry)!
-                            </p>
-                        </div>
-                    </section>
-                </div>
-            </Row>
-        </Container>
+        <div>
+            <Container>
+                <Row>
+                    <div id='aboutMe'>
+                        <section id='aboutMeContainer'>
+                            <Image
+                                src='../public/images/images-tinified/catherine-headshot-sunken-garden.png'
+                                alt='Catherine Smith headshot'
+                                className='headshot'
+                                roundedCircle
+                            />
+                            <div id='aboutMeText'>
+                                <h1><u> About Me </u></h1>
+                                <br></br>
+                                <p>
+                                    My name is Catherine, and I am an emerging full-stack Javascript web developer currently completing the
+                                    University of Richmond's Coding Bootcamp. After earning my BA in Music and Shakespeare Studies
+                                    at the College of William and Mary, I ventured to Glasgow to acquire my MSc in Information and Library Studies at the
+                                    University of Strathclyde. I decided to pursue web development because it's a fast-paced,
+                                    challenging, creative career path with constant opportunities to grow as a person and professional.
+                                </p>
+                                <br></br>
+                                <p>
+                                    While volunteering time as a Library Ambassador at the Earl Gregg Swem Library, I worked with
+                                    the media team to create virtual tours and how-to videos for utilizing library services, and I
+                                    contacted professors and students to act as a reference point if they had any questions about how to
+                                    utilize library resources at William and Mary. Ensuring that people know how to best access and utilize the resources at
+                                    their disposal is a passion of mine. I enjoy studying new subjects, learning German, drinking tea, singing, watching LOTR, and
+                                    listening to audiobooks (particularly if they are narrated by Richard Armitage, Elizabeth Wiley, Jim Dale, or
+                                    Stephen Fry)!
+                                </p>
+                            </div>
+                        </section>
+                    </div>
+                </Row>
+            </Container>
+            <Container>
+                <Technologies />
+            </Container>
+        </div>
     )
 }
 
