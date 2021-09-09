46d76f472e060605984fb11bd3f71c4c7924fd75
crsmith01 <catsmith24601@gmail.com>
2021-09-09 15:24:25 -0400

    installed semantic ui

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'semantic-ui-css/semantic.min.css'
 
 
 
